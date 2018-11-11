ec34595c1ae5682ee4c826e56bf4d7cfb10a0c49
zadiraka <zadirakadeep@gmail.com>
2018-11-11 20:37:06 +0300

    Add files via upload
    you need to choose a language that would work

---
+++ killing floor name dificulty eng , rus/Killing Floor 2 dificulty name Russian/localizationmanager.lua
@@ -0,0 +1,14 @@
+﻿local text_original = LocalizationManager.text
+local testAllStrings = false
+
+function LocalizationManager:text(string_id, ...)
+return string_id == "menu_difficulty_normal" and "Легко"
+    or string_id == "menu_difficulty_hard" and "Легко+"
+    or string_id == "menu_difficulty_very_hard" and "Легко++"
+    or string_id == "menu_difficulty_overkill" and "Средне"
+    or string_id == "menu_difficulty_easy_wish" and "Средне+"
+    or string_id == "menu_difficulty_apocalypse" and "Суицид"
+    or string_id == "menu_difficulty_sm_wish" and "Ад на Земле"
+    or testAllStrings == true and string_id
+    or text_original(self, string_id, ...)
+end
---
+++ killing floor name dificulty eng , rus/Killing Floor 2 dificulty name english/localizationmanager.lua
@@ -0,0 +1,14 @@
+﻿local text_original = LocalizationManager.text
+local testAllStrings = false
+
+function LocalizationManager:text(string_id, ...)
+return string_id == "menu_difficulty_normal" and "Light"
+    or string_id == "menu_difficulty_hard" and "Light+"
+    or string_id == "menu_difficulty_very_hard" and "Light++"
+    or string_id == "menu_difficulty_overkill" and "Average"
+    or string_id == "menu_difficulty_easy_wish" and "Average+"
+    or string_id == "menu_difficulty_apocalypse" and "Suicide"
+    or string_id == "menu_difficulty_sm_wish" and "Hell on earth"
+    or testAllStrings == true and string_id
+    or text_original(self, string_id, ...)
+end
